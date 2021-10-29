44b0b98b733e13529bc42937adebfd2128554c20
Tedd Kourkounakis <tedd.kourkounakis@goldspot.ca>
2021-10-28 23:08:25 -0400

    Added PlayerCount, PlayTime, Weight, and qr code generation

--- BoardGameCatalog/Game.py
+++ BoardGameCatalog/Game.py
@@ -3,6 +3,7 @@ import json
 from libbgg.apiv1 import BGG
 # You can also use version 2 of the api:
 from libbgg.apiv2 import BGG as BGG2
+import qrcode
 
 conn = BGG()
 
@@ -10,15 +11,45 @@ user = "Teddsticle"
 
 # V2
 conn2 = BGG2()
-results = conn2.get_collection(user, own=1)
+results = conn2.get_collection(user, own=1, stats=True)
 # print(json.dumps(results, indent=4, sort_keys=True))
 
+# Link for website
+link_template = "https://boardgamegeek.com/boardgame/{}/"
+image_template = 'C:\\\\Users\\\\teddk\\\\Desktop\\\\Other\\\\BGCatalog\\\\Images\\\\{}.png'
 game_dicts = []
+
 for game in results["items"]["item"]:
+
+    bggid = game["objectid"]
+    print(game['name']['TEXT'])
+
+    #use search for extra stats (weight, description, etc.)
+    try:
+        game_plus = conn2.boardgame(bggid, stats=True)["items"]["item"]
+    except KeyError:
+        continue
+    # print(json.dumps(game_plus, indent=4, sort_keys=True))
+
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+    qr.add_data(link_template.format(bggid))
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    # img.save('test_qrcode.png')
+
     data = {
-        'Image': 'C:\\\\Users\\\\teddk\\\\Desktop\\\\Other\\\\BGCatalog\\\\Images\\\\Keyforge.png',
+        'Image': image_template.format(game['name']['TEXT']),
         'Name': game['name']['TEXT'],
-        'Year': game['yearpublished']['TEXT']
+        'Year': game['yearpublished']['TEXT'],
+        'PlayerCount': game["stats"]["minplayers"] if (game["stats"]["minplayers"] == game["stats"]["maxplayers"])
+                                                   else game["stats"]["minplayers"]+"-"+game["stats"]["maxplayers"],
+        'PlayTime': game["stats"]["minplaytime"] if (game["stats"]["minplaytime"] == game["stats"]["playingtime"])
+                                                   else game["stats"]["minplaytime"]+"-"+game["stats"]["playingtime"],
+        'Weight': game_plus["statistics"]["ratings"]['averageweight']["value"][:4]
     }
     game_dicts.append(data)
 print(game_dicts)
